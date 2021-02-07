ca4b7862bf775ce7fe255d9617a43bc3d5252288
comicodarko <bitotajima@hotmail.com>
2021-02-06 23:28:37 -0300

    Um bocado de texto e alguns problemas resolvidos✅

--- src/pages/AbrirOlhos/index.js
+++ src/pages/AbrirOlhos/index.js
@@ -2,31 +2,273 @@ import React, { useState, useEffect } from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
 
 import { Container } from '../Menu/styles';
-import { NarratorText, Choices, Choice, ChoiceText } from './styles';
+import { zoomIn, zoomOut, NarratorText, Choices, Choice, ChoiceText } from './styles';
 
 export default function AbrirOlhos() {
   
   const [pressDisable, setPressDisable] = useState(true);
-  const [currentText, setCurrentText] = useState(0);
-  const [text, setText] = useState('');
+  const [text, setText] = useState({});
   const [isChoice, setIsChoice] = useState(false);
+  const [currentChoices, setCurrentChoices] = useState([]);
+  const [choicesState, setChoicesState] = useState({});
+  
+  
+  function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex);
+    setText(textNode);
+    setCurrentChoices([]);
+    if(textNode.choices) {
+      setTimeout(() => {
+        textNode.choices.map((choice) => {
+          setCurrentChoices(currentChoices => [...currentChoices, choice]);
+        })
+      }, 1000);
+    } else {
+      setIsChoice(false);
+    }
+  }
 
-  const allTexts = [
+  function selectChoice(choice) {
+    console.log(choice);
+    if(choice.setState) {
+      let states = choicesState;
+      states = Object.assign(states, choice.setState)
+      setChoicesState(states);
+      showTextNode(choice.nextText);
+    } else {
+      showTextNode(choice.nextText);
+    }
+  }
+
+  console.log(choicesState);
+
+  const textNodes = [
     {
-      text: 'É uma sala escura, nela não há mobílias, em sua plena escuridão nada pode ser visto...', 
-      choice: false,
+      id: 1,
+      text: 'É uma sala escura, nela não há mobílias, em sua plena escuridão nada pode ser visto...',
+      nextText: 2
     },
     {
+      id: 2,
       text: 'Me levanto...',
-      choice: true,
       choices: [
         {
-          choiceText: 'toco meu rosto',
-          nextText: 'Seus olhos estão fechados.'
+          text: 'toco meu rosto',
+          nextText: 2.1,
         },
         {
-          choiceText: 'encaro o abismo',
-          nextText: 'Ele te encara de volta, o medo corrói cada parte do seu ser, você não está sozinho, está com o abismo.'
+          text: 'encaro o abismo', //Refletirá no futuro
+          nextText: 2.2,
+          setState: { encarouAbismo: true }
+        }
+      ]
+    },
+    {
+      id: 2.1,
+      text: 'Seus olhos estão fechados.',
+      choices: [
+        {
+          text: 'abrir os olhos',
+          nextText: 3
+        }
+      ]
+    },
+    {
+      id: 2.2,
+      text: 'Ele te encara de volta, o medo corrói cada parte do seu ser, você não está sozinho, está com o abismo.',
+      nextText: 3
+    },
+    {
+      id: 3,
+      text: 'As luzes acendem, há uma porta, uma caixa escrita “Lembranças” e um espelho quebrado em 3 partes.',
+      choices: [
+        {
+          text: 'ir até a porta',
+          nextText: 3.1
+        },
+        {
+          text: 'ir até a caixa',
+          nextText: 3.2
+        },
+        {
+          text: 'ir ao espelho',
+          nextText: 4
+        }
+      ]
+    },
+    {
+      id: 3.1,
+      text: 'A porta está trancada.',
+      choices: [
+        {
+          text: 'ir até a caixa',
+          nextText: 3.2
+        },
+        {
+          text: 'ir até o espelho',
+          nextText: 4
+        }
+      ],
+    },
+    {
+      id: 3.2,
+      text: 'A caixa está lacrada.',
+      choices: [
+        {
+          text: 'ir até a porta',
+          nextText: 3.1
+        },
+        {
+          text: 'ir até o espelho',
+          nextText: 4
+        }
+      ]
+    },
+    {
+      id: 4,
+      text: 'O reflexo corrompido distorce sua sombra, e do ângulo certo, há um buraco em seu peito.',
+      choices: [
+        {
+          text: 'fitar a sombra',
+          nextText: 4.1,
+        },
+        {
+          text: 'enfiar a mão no buraco do peito',
+          nextText: 4.2
+        },
+        {
+          text: 'pegar caco de vidro',
+          nextText: 4.3
+        }
+      ]
+    },
+    {
+      id: 4.1,
+      text: `${choicesState.encarouAbismo ? 'Então você finalmente resolve abrir os olhos, ver a verdade do ser é sempre o primeiro passo.' : '-Oh... Olá amigo, já está acordado? Fico feliz que voltou.'}`,
+      choices: [
+        {
+          text: 'enfiar a mão no buraco do peito',
+          nextText: 4.2
+        },
+        {
+          text: 'pegar caco de vidro',
+          nextText: 4.3
+        }
+      ]
+    },
+    {
+      id: 4.2,
+      text: 'Você encontrou uma chave!', 
+      choices: [
+        {
+          text: 'sair do quarto',
+          nextText: 5
+        },
+        {
+          text: 'Voltar a encarar o espelho',
+          nextText: 4.21
+        }
+      ]
+    },
+    {
+      id: 4.21,
+      text: '',
+      choices: [
+        {
+          text: 'pegar caco de vidro',
+          nextText: 4.3
+        }
+      ]
+    },
+    {
+      id: 4.3,
+      text: '',
+      choices: [
+        {
+          text: 'cortar jugular',
+          nextText: 4.31 
+        },
+        {
+          text: 'atacar a sombra',
+          nextText: 4.32
+        },
+        {
+          text: 'ir até a caixa',
+          nextText: 4.33 
+        },
+      ]
+    },
+    {
+      id: 4.31,
+      text: 'Você não teve forças para abrir a porta e está tudo bem...',
+      choices: [
+        {
+          text: 'abrir os olhos',
+          nextText: 4.311
+        } 
+      ]
+    },
+    {
+      id: 4.311,
+      text: 'Tudo a de ficar melhor, até o mais profundo dos abismos tem um final... Caso esteja se sentindo deprimido ou com desejo suicida, disque 188 ou acesse o site https://www.cvv.org.br, com todo amor, BS Studios!',
+      nextText: 4.3
+    },
+    {
+      id: 4.32,
+      text: 'Ao atacar a sombra, o espelho se parte em centenas de pedaços.',
+      nextText: 4.321
+    },
+    {
+      id: 4.321,
+      text: '-Acho que até os melhores amigos brigam, tudo bem, vou te deixar sozinho por um tempo.',
+      choices: [
+        {
+          text: 'ir até a caixa',
+          nextText: 4.33 
+        },
+      ]
+    },
+    {
+      id: 4.33,
+      text: '',
+      choices: [
+        {
+          text: 'deixar a caixa no chão', // Não será possível mais acessar caixa na run
+          nextText: 4.331         
+        },
+        {
+          text: 'abrir a caixa',
+          nextText: 4.332
+        }
+      ]
+    },
+    {
+      id: 4.331,
+      text: '“Algumas vezes, a vastidão do esquecimento é mais confortável que a o peso da realidade.”',
+      choices: [
+        {
+          text: 'sair do quarto',
+          nextText: 5
+        }
+      ]
+    },
+    {
+      id: 4.332,
+      text: 'O medo primal invade porta a dentro, a dor das escolhas consomem tua sanidade, o medo e depressão voltam a tomar conta.',
+      nextText: 4.333,
+    },
+    {
+      id: 4.333,
+      text: '-E foi por isso que escolhemos por tudo nessa caixa amigo, não aguentamos mais tal peso, feche a caixa, retorne ao esquecimento, retorne ao conforto da vastidão.',
+      nextText: 4.334,
+    },
+    {
+      id: 4.334,
+      text: 'Você abre seus olhos, o loop não recomeça, a caixa está lacrada com mais uma camada de fita, o espelho está intacto e a porta completamente aberta.',
+      choices: [
+        {
+          text: 'fechar a caixa e fechar os olhos',
+          // nextText: 
         }
       ]
     }
@@ -34,96 +276,49 @@ export default function AbrirOlhos() {
 
   useEffect(() => {
     setPressDisable(true);
-    setCurrentText(0);
     setIsChoice(false);
     setTimeout(() => {
-      setText(allTexts[currentText].text);
+      showTextNode(1);
     }, 2000)
   }, [])
-
-  function showTextNode(textNodeIndex) {
-
-  }
-
-  function selectOption(option) {
-
-  }
-
-
-
-  const zoomIn = {
-    0: {
-      opacity: 0,
-      scale: 0,
-    },
-    0.5: {
-      opacity: 1,
-      scale: 0.3,
-    },
-    1: {
-      opacity: 1,
-      scale: 1,
-    },
-  };
-
-  const zoomOut = {
-    0: {
-      opacity: 1,
-      scale: 1,
-    },
-    0.5: {
-      opacity: 1,
-      scale: 0.3,
-    },
-    1: {
-      opacity: 0,
-      scale: 0,
-    },
-  }
-
-  useEffect(() => {
-    setText(allTexts[currentText].text)
-    allTexts[currentText].choice ? setPressDisable(true) : setPressDisable(false);
-  }, [currentText])
 
   return (
     <Container>
       <TouchableWithoutFeedback
         onPress={() =>  {
-          setPressDisable(true);
-          if(!pressDisable){
-            console.log(pressDisable);
-            currentText < allTexts.length -1 && setCurrentText(currentText + 1);
-          }
+          if(currentChoices.length > 0) {
+            return  
+          } else if(!pressDisable){
+            setPressDisable(true);
+            if(!isChoice) {
+              showTextNode(text.nextText); 
+            }
+          } 
         }
       }
       >
         <NarratorText
-          minDelay={20} maxDelay={80}
+          // minDelay={20} maxDelay={80}
+          minDelay={10} maxDelay={10}
           typing={1} fixed={true}
-          onTyped={() => setPressDisable(true)}
-          onTypingEnd={() => {
-            setPressDisable(false);
-            setTimeout(() => {
-              allTexts[currentText].choice ? setIsChoice(true) : setIsChoice(false);  
-            }, 1500);
-            }}
+          onTyped={() => {setPressDisable(true); setIsChoice(false)}}
+          onTypingEnd={() => {setPressDisable(false); !isChoice && text.choices && setIsChoice(true);}}
         >
-          {text}
+          {text.text !== undefined ? text.text : ''}
         </NarratorText> 
       </TouchableWithoutFeedback>
-      {isChoice &&
+   
         <Choices animation={isChoice ? zoomIn : zoomOut}>
-          {allTexts[currentText].choices.map((choice) => {
+          {currentChoices.map((choice) => {
             return (
-              <Choice key={choice.choiceText}
-                onPress={() => setText(choice.nextText)}
+              <Choice
+                key={choice.text}
+                onPress={() => selectChoice(choice)}
               >
-                <ChoiceText>{choice.choiceText}</ChoiceText>
+                <ChoiceText>{choice.text}</ChoiceText>
               </Choice>
-            )
-          })}
-        </Choices>}
+          )})}
+        </Choices>
     </Container>
   );
 };
--- src/pages/AbrirOlhos/styles.js
+++ src/pages/AbrirOlhos/styles.js
@@ -3,6 +3,36 @@ import TypeWriter from 'react-native-typewriter'
 import * as Animatable from 'react-native-animatable';
 import normalize from '../../utils/normalize';
 
+export const zoomIn = {
+  0: {
+    opacity: 0,
+    scale: 0,
+  },
+  0.5: {
+    opacity: 1,
+    scale: 0.3,
+  },
+  1: {
+    opacity: 1,
+    scale: 1,
+  },
+};
+
+export const zoomOut = {
+  0: {
+    opacity: 1,
+    scale: 1,
+  },
+  0.5: {
+    opacity: 1,
+    scale: 0.3,
+  },
+  1: {
+    opacity: 0,
+    scale: 0,
+  },
+}
+
 export const NarratorText = styled(TypeWriter)`
   font-family: 'PressStart2P-Regular';
   color: #fff;
@@ -16,9 +46,8 @@ export const Choices = styled(Animatable.View)`
   position: absolute;
   bottom: ${normalize(50)}px;
   background-color: #fff2;
-  max-height: ${normalize(200)}px;
   margin-top: ${normalize(100)}px;
-  padding: ${normalize(10)}px;
+  padding: ${normalize(5)}px ${normalize(10)}px;
   justify-content: space-around;
   width: 100%;
   border-radius: 5px;
@@ -28,13 +57,13 @@ export const Choice = styled.TouchableOpacity`
   width: 100%;
   background-color: #aaa;
   padding: ${normalize(20)}px ${normalize(5)}px;
-  margin-bottom: ${normalize(10)}px;
-  margin-top: ${normalize(10)}px;
+  margin-bottom: ${normalize(5)}px;
+  margin-top: ${normalize(5)}px;
   border-radius: 5px;
   justify-content: center;
   align-items: center;
 `
 
 export const ChoiceText = styled(DefaultText)`
-
+  text-align: center;
 `
